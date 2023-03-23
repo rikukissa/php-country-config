8df551267c14b6081933f95a6d71d0d54d743350
Riku Rouvila <riku.rouvila@gmail.com>
2023-03-23 21:09:40 +0100

    add a registrar account

--- seed/seed.php
+++ seed/seed.php
@@ -6,13 +6,13 @@ $client = new MongoDB\Client('mongodb://localhost:27017');
 
 
 
-$client->selectDatabase('user-mgnt')->selectCollection('users')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/users.json')));
-$client->selectDatabase('user-mgnt')->selectCollection('roles')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/roles.json')));
-$client->selectDatabase('hearth-dev')->selectCollection('Practitioner')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/practitioners.json')));
+$client->selectDatabase('user-mgnt')->selectCollection('users')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/users/users.json')));
+$client->selectDatabase('user-mgnt')->selectCollection('roles')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/users/roles.json')));
+$client->selectDatabase('hearth-dev')->selectCollection('Practitioner')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/users/practitioners.json')));
 
+$client->selectDatabase('hearth-dev')->selectCollection('Location')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/locations/locations.json')));
+$client->selectDatabase('hearth-dev')->selectCollection('Location')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/locations/offices.json')));
+$client->selectDatabase('hearth-dev')->selectCollection('Location')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/locations/health-facilities.json')));
 
-$client->selectDatabase('hearth-dev')->selectCollection('Location')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/locations.json')));
-$client->selectDatabase('hearth-dev')->selectCollection('Location')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/offices.json')));
-$client->selectDatabase('hearth-dev')->selectCollection('Location')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/health-facilities.json')));
 $client->selectDatabase('application-config')->selectCollection('configs')->insertOne(json_decode(file_get_contents(__DIR__ . '/data/application-configuration.json')));
 $client->selectDatabase('application-config')->selectCollection('certificates')->insertMany(json_decode(file_get_contents(__DIR__ . '/data/certificates.json')));
